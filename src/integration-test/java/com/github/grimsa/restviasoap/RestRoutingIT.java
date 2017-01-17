881066bcc0f1c3633bff33ec904238848c9932fa
grimsa <gediminas.rimsa@gmail.com>
2017-01-17 23:29:25 +0200

    Simplify integration test setup; first working test

--- src/integration-test/java/com/github/grimsa/restviasoap/RestRoutingIT.java
+++ src/integration-test/java/com/github/grimsa/restviasoap/RestRoutingIT.java
@@ -1,14 +1,23 @@
 package com.github.grimsa.restviasoap;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.EnumSet;
+import java.util.Optional;
 
 import javax.servlet.DispatcherType;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
 
+import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -16,6 +25,8 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.github.grimsa.restviasoap.generated.HttpMethod;
+import com.github.grimsa.restviasoap.generated.Response;
 import com.google.common.io.Resources;
 
 import io.restassured.RestAssured;
@@ -45,15 +56,53 @@ public class RestRoutingIT {
     }
 
     @Test
-    public void testGetRequest() throws IOException {
+    public void shouldWriteResponseToWriter() throws IOException {
         // given
-        String soapRequest = Resources.toString(Resources.getResource("requests/GET.xml"), StandardCharsets.UTF_8);
+        String soapRequest = givenSoapRequest(HttpMethod.GET, TestServlet.PATH_REQUEST_WRITES_TO_WRITER, null);
 
         // when
-        String soapResponse = given().request().contentType("application/soap+xml; charset=UTF-8;").body(soapRequest).post("/soap-api").andReturn().asString();
+        String soapResponse = whenSoapMessageIsSent(soapRequest);
 
         // response
-        assertTrue(soapResponse.contains("status=\"200\""));
+        thenResponseIs(soapResponse, HttpStatus.OK_200, TestServlet.RESPONSE_BODY);
+    }
+
+    @Test
+    public void shouldWriteResponseToStream() throws IOException {
+        // given
+        String soapRequest = givenSoapRequest(HttpMethod.GET, TestServlet.PATH_REQUEST_WRITES_TO_STREAM, null);
+
+        // when
+        String soapResponse = whenSoapMessageIsSent(soapRequest);
+
+        // response
+        thenResponseIs(soapResponse, HttpStatus.OK_200, TestServlet.RESPONSE_BODY);
+    }
+
+    private String givenSoapRequest(HttpMethod httpMethod, String path, String body) throws IOException {
+        String template = Resources.toString(Resources.getResource("templates/request.xml"), StandardCharsets.UTF_8);
+        return template.replace("${method}", httpMethod.name()).replace("${path}", path).replace("${body}", Optional.ofNullable(body).orElse(""));
+    }
+
+    private String whenSoapMessageIsSent(String soapRequest) {
+        return given().request().contentType("application/soap+xml; charset=UTF-8;").body(soapRequest).post("/soap-api").andReturn().asString();
+    }
+
+    private void thenResponseIs(String soapResponse, int httpStatus, String body) {
+        Response restResponse = extractRestResponse(soapResponse);
+        assertEquals(HttpStatus.OK_200, restResponse.getStatus());
+        assertEquals(body, restResponse.getValue());
+    }
+
+    private Response extractRestResponse(String soapResponse) {
+        try {
+            ByteArrayInputStream soapResponseIs = new ByteArrayInputStream(soapResponse.getBytes(StandardCharsets.UTF_8));
+            SOAPMessage message = MessageFactory.newInstance().createMessage(null, soapResponseIs);
+            Unmarshaller unmarshaller = JAXBContext.newInstance(Response.class).createUnmarshaller();
+            return (Response) unmarshaller.unmarshal(message.getSOAPBody().extractContentAsDocument());
+        } catch (JAXBException | SOAPException | IOException e) {
+            throw new AssertionError("Failed to extract Response from SOAP message: " + soapResponse, e);
+        }
     }
 
 }
--- src/integration-test/java/com/github/grimsa/restviasoap/TestServlet.java
+++ src/integration-test/java/com/github/grimsa/restviasoap/TestServlet.java
@@ -10,9 +10,42 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    static final String PATH_REQUEST_WRITES_TO_WRITER = "/api/user/200/1";
+    static final String PATH_REQUEST_WRITES_TO_STREAM = "/api/user/200/2";
+    static final String PATH_REQUEST_EMPTY_BODY = "/api/user/200/3";
+    static final String PATH_REQUEST_SENDS_ERROR = "/api/user/404";
+    static final String PATH_REQUEST_SENDS_ERROR_WITH_MESSAGE = "/api/user/404/msg";
+    static final String PATH_REQUEST_SENDS_REDIRECT = "/api/user/302";
+    static final String PATH_REQUEST_THROWS_EXCEPTION = "/api/user/500";
+    static final String RESPONSE_BODY = "{ 'id' : '000', 'name' : 'Bob' }";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("Hello");
+        switch(req.getRequestURI().substring(req.getContextPath().length())) {
+        case PATH_REQUEST_WRITES_TO_WRITER:
+            resp.getWriter().print(RESPONSE_BODY);
+            resp.getWriter().close();
+            break;
+
+        case PATH_REQUEST_WRITES_TO_STREAM:
+            resp.getOutputStream().print(RESPONSE_BODY);
+            break;
+
+        case PATH_REQUEST_SENDS_ERROR:
+            resp.sendError(404);
+            break;
+
+        case PATH_REQUEST_SENDS_ERROR_WITH_MESSAGE:
+            resp.sendError(404, "Boom, not found!");
+            break;
+
+        case PATH_REQUEST_SENDS_REDIRECT:
+            resp.sendRedirect(PATH_REQUEST_WRITES_TO_STREAM);
+            break;
+
+        default:
+            throw new IllegalStateException("Something failed in application logic!");
+        }
     }
 
 }
